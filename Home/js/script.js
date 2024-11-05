feab89037bd17ec6f63000e3fef48d77eaab8897
Yuu <152776955+yuukun123@users.noreply.github.com>
2024-11-05 21:19:30 +0700

    Add files via upload

--- Home/js/script.js
+++ Home/js/script.js
@@ -133,7 +133,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (user) {
             alert('Login successful!');
             setCurrentUser(user);
-            window.location.href = '../Client/index.html';
+            window.location.href = '../../Client/ClientPage/index.html';
         } else {
             alert('Invalid email or password!');
         }
@@ -167,7 +167,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Alert and redirect to login
             alert('Registration successful!');
-            window.location.href = '../index.htm';
+            window.location.href = './index.htm';
         }
     });
 
@@ -208,28 +208,28 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sample data structure for items
     const menuItems = {
         Mousse: [
-            { links: 'Food_Infor/index-1.html', name: 'Avocado Mousse', price: '510,000 VND', image: 'img/Mousse/Avocado_Mousse.jpg' },
-            { links: 'Food_Infor/index-2.html', id: '2', name: 'Blueberry Mousse', price: '510,000 VND', image: 'img/Mousse/Blueberry_Mousse.jpg' },
-            { id: '3', name: 'Corn Mousse', price: '520,000 VND', image: 'img/Mousse/Corn_Mousse.jpg' },
-            { id: '4', name: 'Longan Mousse', price: '530,000 VND', image: 'img/Mousse/Longan_Mousse.jpg' },
-            { id: '5', name: 'Mango Mousse', price: '540,000 VND', image: 'img/Mousse/Mango_Mousse.jpg' },
-            { id: '6', name: 'Melon Mousse', price: '550,000 VND', image: 'img/Mousse/Melon_Mousse.jpg'},
+            { links: '../Food_Infor/index-1.html', name: 'Avocado Mousse', price: '510,000 VND', image: '../Img/Mousse/Avocado_Mousse.jpg' },
+            { links: '../Food_Infor/index-2.html', id: '2', name: 'Blueberry Mousse', price: '510,000 VND', image: '../Img/Mousse/Blueberry_Mousse.jpg' },
+            { links: '../Food_Infor/index-3.html', id: '3', name: 'Corn Mousse', price: '520,000 VND', image: '../Img/Mousse/Corn_Mousse.jpg' },
+            { links: '../Food_Infor/index-4.html', id: '4', name: 'Longan Mousse', price: '530,000 VND', image: '../Img/Mousse/Longan_Mousse.jpg' },
+            { links: '../Food_Infor/index-5.html', id: '5', name: 'Mango Mousse', price: '540,000 VND', image: '../Img/Mousse/Mango_Mousse.jpg' },
+            { links: '../Food_Infor/index-6.html', id: '6', name: 'Melon Mousse', price: '550,000 VND', image: '../Img/Mousse/Melon_Mousse.jpg'},
         ],
         Croissant: [
-            { id: '7', name: 'Avocado Croissant', price: '110,000 VND', image: 'img/Croissant/Avocado_Croissant.jpg' },
-            { id: '8', name: 'Choco Mallow Croissant', price: '110,000 VND', image: 'img/Croissant/Choco_Mallow_Croissant.png' },
-            { id: '9', name: 'Dinosaur Almond Croissant', price: '120,000 VND', image: 'img/Croissant/Dinosaur_Almond_Croissant.png' },
-            { id: '10', name: 'Honey Almond Croissant', price: '130,000 VND', image: 'img/Croissant/Honey_Almond_Croissant.png' },
-            { id: '11', name: 'Matcha Croissant', price: '140,000 VND', image: 'img/Croissant/Matcha_Croissant.jpg' },
-            { id: '12', name: 'Plain Croissant', price: '150,000 VND', image: 'img/Croissant/Plain_Croissant.png' },
+            { links: '../Food_Infor/index-7.html', id: '7', name: 'Avocado Croissant', price: '110,000 VND', image: '../Img/Croissant/Avocado_Croissant.jpg' },
+            { links: '../Food_Infor/index-8.html', id: '8', name: 'Choco Mallow Croissant', price: '110,000 VND', image: '../Img/Croissant/Choco_Mallow_Croissant.png' },
+            { links: '../Food_Infor/index-9.html', id: '9', name: 'Dinosaur Almond Croissant', price: '120,000 VND', image: '../Img/Croissant/Dinosaur_Almond_Croissant.png' },
+            { links: '../Food_Infor/index-10.html', id: '10', name: 'Honey Almond Croissant', price: '130,000 VND', image: '../Img/Croissant/Honey_Almond_Croissant.png' },
+            { links: '../Food_Infor/index-11.html', id: '11', name: 'Matcha Croissant', price: '140,000 VND', image: '../Img/Croissant/Matcha_Croissant.jpg' },
+            { links: '../Food_Infor/index-12.html', id: '12', name: 'Plain Croissant', price: '150,000 VND', image: '../Img/Croissant/Plain_Croissant.png' },
         ],
         Drink: [
-            { id: '13', name: 'Choco Mallow', price: '55,000 VND', image: 'img/Drink/Choco_Mallow.png' },
-            { id: '14', name: 'Lemon Tea', price: '60,000 VND', image: 'img/Drink/Lemon_Tea.png' },
-            { id: '15', name: 'Lychee Tea', price: '70,000 VND', image: 'img/Drink/Lychee_Tea.png' },
-            { id: '16', name: 'Matcha Latte', price: '75,000 VND', image: 'img/Drink/Matcha_Latte.png' },
-            { id: '17', name: 'Matcha Mallow', price: '80,000 VND', image: 'img/Drink/Matcha_Mallow .png' },
-            { id: '18', name: 'Matcha Misu', price: '85,000 VND', image: 'img/Drink/Matcha_Misu.png' },
+            { links: '../Food_Infor/index-13.html', id: '13', name: 'Choco Mallow', price: '55,000 VND', image: '../Img/Drink/Choco_Mallow.png' },
+            { links: '../Food_Infor/index-14.html', id: '14', name: 'Lemon Tea', price: '60,000 VND', image: '../Img/Drink/Lemon_Tea.png' },
+            { links: '../Food_Infor/index-15.html', id: '15', name: 'Lychee Tea', price: '70,000 VND', image: '../Img/Drink/Lychee_Tea.png' },
+            { links: '../Food_Infor/index-16.html', id: '16', name: 'Matcha Latte', price: '75,000 VND', image: '../Img/Drink/Matcha_Latte.png' },
+            { links: '../Food_Infor/index-17.html', id: '17', name: 'Matcha Mallow', price: '80,000 VND', image: '../Img/Drink/Matcha_Mallow .png' },
+            { links: '../Food_Infor/index-18.html', id: '18', name: 'Matcha Misu', price: '85,000 VND', image: '../Img/Drink/Matcha_Misu.png' },
         ]
     };
 
@@ -292,37 +292,37 @@ document.addEventListener('DOMContentLoaded', function() {
             </button>
         `;
 
-        // Function to get current user from localStorage
-        function getCurrentUser() {
-            const currentUser = localStorage.getItem('UserStr');
-            return currentUser ? JSON.parse(currentUser) : null;
+        // // Function to get current user from localStorage
+        // function getCurrentUser() {
+        //     const currentUser = localStorage.getItem('UserStr');
+        //     return currentUser ? JSON.parse(currentUser) : null;
 
-        }
+        // }
 
-        function isLoggedIn() {
-            return !!getCurrentUser(); // Returns true if currentUser exists, false otherwise
-        }
+        // function isLoggedIn() {
+        //     return !!getCurrentUser(); // Returns true if currentUser exists, false otherwise
+        // }
 
-        // const cartBtn = document.getElementById('cart-btn');
-        const cartBtn = document.querySelectorAll('.sp-cart')
-        if (cartBtn.length > 0) {
-            cartBtn.forEach(button => {
-                button.addEventListener('click', function(event) {
-                    event.preventDefault();
+        // // const cartBtn = document.getElementById('cart-btn');
+        // const cartBtn = document.querySelectorAll('.sp-cart')
+        // if (cartBtn.length > 0) {
+        //     cartBtn.forEach(button => {
+        //         button.addEventListener('click', function(event) {
+        //             event.preventDefault();
                     
-                    if (!isLoggedIn()) {
-                        // alert('Please log in to view your cart!');
-                        wrapper.classList.add('active-popup');
-                        wrapper.classList.remove('active');
-                        isRegisterForm = false;
-                        blurOverlay.classList.add('active');
-                    } else {
-                        // Code to view cart goes here (if user is logged in)
-                        console.log('Viewing cart...'); // Placeholder for cart viewing logic
-                    }
-                });
-            });
-        }
+        //             if (!isLoggedIn()) {
+        //                 // alert('Please log in to view your cart!');
+        //                 wrapper.classList.add('active-popup');
+        //                 wrapper.classList.remove('active');
+        //                 isRegisterForm = false;
+        //                 blurOverlay.classList.add('active');
+        //             } else {
+        //                 // Code to view cart goes here (if user is logged in)
+        //                 console.log('Viewing cart...'); // Placeholder for cart viewing logic
+        //             }
+        //         });
+        //     });
+        // }
 
         return card;
     }
@@ -573,9 +573,23 @@ window.addEventListener('resize', handleScroll);
 // Initial call to set the correct state
 handleScroll();
 
-/*search*/
-// Function to display filtered menu items based on the search query
+// back to top scrolling
+window.onscroll = function () {
+    toggleBackToTopButton();
+};
 
+function toggleBackToTopButton() {
+    const backToTopButton = document.getElementById("backToTop");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
 
 $(document).ready(function(){
